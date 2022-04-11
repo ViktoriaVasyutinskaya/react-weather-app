eba61be042c93b119d1d10c060ad7b00794be688
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-04-11 21:10:12 +0200

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <small>
+      <a
+        href="https://github.com/ViktoriaVasyutinskaya/vanilla-weather-app/commit/104f970a2d10f0ce543d1a45b15d9b30f37a0056"
+        id="openSource"
+      >
+        Open-source code&nbsp;
+      </a>
+      by V.Vasyutinskaya
+    </small>
+  );
+}
