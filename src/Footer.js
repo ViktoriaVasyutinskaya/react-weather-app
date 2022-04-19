848e43d0139e04de30741382ae9c8a4b44460b64
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-04-19 12:30:33 +0200

    added links and default styling

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,27 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <small>
-      <a
-        href="https://github.com/ViktoriaVasyutinskaya/vanilla-weather-app/commit/104f970a2d10f0ce543d1a45b15d9b30f37a0056"
-        id="openSource"
-      >
-        Open-source code&nbsp;
-      </a>
-      by V.Vasyutinskaya
-    </small>
+    <div className="footer">
+      <small>
+        <a
+          href="https://github.com/ViktoriaVasyutinskaya/react-weather-app"
+          id="openSource"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://splendorous-tulumba-70ea6d.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="netlify"
+        >
+          hosted on Netlify
+        </a>{" "}
+        by V.Vasyutinskaya
+      </small>
+    </div>
   );
 }
