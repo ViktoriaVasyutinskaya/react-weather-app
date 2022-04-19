556a72b606b21158f15fcd5cd7f2fcf5be180757
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-04-19 12:33:49 +0200

    used bootstrap classes for grid

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="row">
+    <div className="mt-1">
       <ul>
         <li id="city">{props.data.city}</li>
         <li id="date">Last updated:</li>
       </ul>
-      <div className="Conditions">
-        <span className="current-conditions">
+      <div className="row Conditions">
+        <div className="col-6 current-conditions">
           <img
             className="weather-icon"
             src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`}
@@ -17,12 +17,14 @@ export default function WeatherInfo(props) {
           <strong id="currentTemp">
             {Math.round(props.data.temperature)}°C
           </strong>{" "}
-          <ul className="description">
+        </div>
+        <div className="col-6 mb-3 description">
+          <ul>
             <li>{props.data.description}</li>
             <li>Humidity: {Math.round(props.data.humidity)}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+            <li>Wind: {Math.round(props.data.wind)}km/h</li>
           </ul>
-        </span>
+        </div>
       </div>
     </div>
   );
